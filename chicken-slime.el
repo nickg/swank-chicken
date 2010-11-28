cd6ea2f7d0a73ce07eae95bc62af848e76321b2c
Nick Gasson <nick@nickg.me.uk>
2010-11-28 15:08:42 +0000

    Add an Emacs Lisp wrapper function chicken-slime

---
+++ chicken-slime.el
@@ -0,0 +1,19 @@
+(setq slime-lisp-implementations
+      (cons '(chicken ("csi")
+                      :init chicken-slime-init)
+            slime-lisp-implementations))
+
+(defun chicken-slime-init (file _)
+  (setq slime-protocol-version 'ignore)
+  (let ((swank "/home/nick/swank-chicken/swank-chicken.scm"))
+    (format "%S\n"
+            `(begin (require 'tcp)
+                    (require 'posix)
+                    (load ,swank) 
+                    (swank-server-start 4005, file)))))
+
+(defun chicken-slime ()
+  (interactive)
+  (slime "chicken"))
+
+(provide 'slime-slime)
