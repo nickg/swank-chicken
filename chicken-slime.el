f24962a3d36dd9320159d8cfa2065a1955d562f9
Nick Gasson <nick@nickg.me.uk>
2011-01-01 14:34:56 +0000

    Wrap code in a module

--- chicken-slime.el
+++ chicken-slime.el
@@ -3,15 +3,19 @@
                       :init chicken-slime-init)
             slime-lisp-implementations))
 
-(defvar swank-chicken-path "/path/to/swank.scm")
+(defvar swank-chicken-path nil
+  "Path to swank.scm. Set to nil to use installed extension.")
 
 (defun chicken-slime-init (file _)
   (setq slime-protocol-version 'ignore)
   (setq slime-complete-symbol-function 'slime-simple-complete-symbol)
-  (let ((swank swank-chicken-path))
-    (format "%S\n"
-            `(begin (load ,swank) 
-                    (swank-server-start 4005 ,file)))))
+  
+  (format "%S\n"
+          `(begin ,@(if swank-chicken-path
+                        `((load ,swank-chicken-path)   ; Interpet code for testing
+                          (import swank))
+                     '((require-extension swank)))     ; Normal use
+                  (swank-server-start 4005 ,file))))
 
 (defun chicken-slime ()
   (interactive)
--- swank.scm
+++ swank.scm
@@ -20,6 +20,44 @@
 ;;; DEALINGS IN THE SOFTWARE.
 ;;;
 
+(module swank (swank-server-start
+               swank:connection-info
+               swank:swank-require
+               swank:autodoc
+               swank:create-repl
+               swank:listener-eval
+               swank:compile-string-for-emacs
+               swank:interactive-eval
+               swank:interactive-eval-region
+               swank:pprint-eval
+               swank:operator-arglist
+               swank:throw-to-toplevel
+               swank:invoke-nth-restart-for-emacs
+               swank:backtrace
+               swank:load-file
+               swank:autodoc
+               swank:simple-completions
+               swank:buffer-first-change
+               swank:filename-to-modulename
+               swank:find-definitions-for-emacs
+               swank:swank-require)
+  (import scheme
+          chicken)
+  (use ports
+       tcp
+       posix
+       srfi-1
+       srfi-13
+       symbol-utils
+       apropos
+       fmt)
+  
+(require 'tcp)
+(require 'posix)
+(require-extension symbol-utils
+                   apropos
+                   fmt)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -35,12 +73,6 @@
 ;;;   M-x slime-connect
 ;;; And accept the default options.
 ;;;
-
-(require 'tcp)
-(require 'posix)
-(require-extension symbol-utils
-                   apropos
-                   fmt)
 
 ;; Safe printing routine that always uses the server's standard output.
 (define debug-print
@@ -430,3 +462,5 @@
 (define t #t)
 
 (define :print-right-margin 'print-right-margin)
+
+)   ; module swank
